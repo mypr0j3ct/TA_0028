ea1c275e30195da06a5472e23df515b5de42217c
mypr0j3ct <123tychan@gmail.com>
2025-06-30 01:06:50 +0700

    Create 1_single_interrtupta.cpp

---
+++ avr/1_single_interrtupta.cpp
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+ISR(INT0_vect)
+{
+    PORTB |= (1<<7);
+    _delay_ms(200);
+    PORTB &= ~(1<<7);
+}
+
+int main (void)
+{
+    DDRA=0xff;
+    DDRB |= (1<<PB7); // LED output enable
+    PORTD |= _BV(PD0); // pin int0 PD0 pullup
+    EIMSK |= (1<<INT0); // enable interrupt
+    EICRA |= _BV(ISC01) | _BV(ISC00); // rising edge
+    sei();
+
+    while (1)
+    {
+        PORTA=0x00;
+        for (unsigned char i=0;i<8;i++)
+        {
+            PORTA = ~(1<<i);
+            _delay_ms(500);
+        }
+    }
+}
